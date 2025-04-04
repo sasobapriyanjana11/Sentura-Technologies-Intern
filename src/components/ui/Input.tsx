ec9dd24a4a2b11ea0879158869d54e9c2e5d736b
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-04-04 09:42:01 +0530

    Input,Button components created

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,4 @@
+const Input = ({ ...props }) => (
+    <input className="border px-3 py-2 rounded" {...props} />
+);
+export default Input;
---
+++ src/components/ui/Table.tsx
@@ -0,0 +1,4 @@
+ const Table = ({ children }) => (
+    <table className="w-full border-collapse">{children}</table>
+);
+ export default Table;
